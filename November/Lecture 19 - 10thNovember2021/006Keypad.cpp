7a41afa1b31290897646d0857fe08089efec5db1
abhishek18124 <abhishek18124@iiitd.ac.in>
2021-11-10 20:59:33 +0530

    Lecture 19

---
+++ November/Lecture 19 - 10thNovember2021/006Keypad.cpp
@@ -0,0 +1,24 @@
+/*
+
+Given a phone keypad as shown below, and an n digit number, print all words which are possible by 
+pressing the n digits on the keypad.
+
+ 1()     2(abc) 3(def) 
+ 4(ghi)  5(jkl) 6(mno)
+ 7(pqrs) 8(tuv) 9(wxyz)
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+string keypad[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+int main() {
+	
+    char inp[] = "234";
+    char out[10];
+
+	return 0;
+}
