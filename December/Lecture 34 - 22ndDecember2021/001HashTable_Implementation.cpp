c8cd4e030ab66387919671b8c7d669834f70d81c
abhishek18124 <abhishek18124@iiitd.ac.in>
2021-12-22 22:07:12 +0530

    Lecture 34

---
+++ December/Lecture 34 - 22ndDecember2021/001HashTable_Implementation.cpp
@@ -0,0 +1,231 @@
+/*
+
+	Implementation of HashTable
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+template <typename T1, typename T2>
+class node {
+
+public:
+	T1 key;	
+	T2 value;
+	node<T1, T2>* next;
+
+	node(T1 key, T2 value) {
+		this->key = key;
+		this->value = value;
+		this->next = NULL;
+	}
+};
+
+template <typename T1, typename T2>
+class HashTable {
+	int M; // to store the capacity of the hash table
+	int N; // to store the size i.e. no. of key value pairs inside the hash table
+	double LFT; // to store the load factor threshold
+	node<T1, T2>** table; // to store the address of the dynamic array that represents the table
+
+	int hash_fn(T1 key) {
+		return key%M;
+	}
+
+	void transfer(node<T1, T2>* head) {
+		node<T1, T2>* temp = head;
+		while(temp) {
+			insert(temp->key, temp->value);
+			temp = temp->next;
+		}
+
+		// release the memory for the linked list at the ith index
+		
+		while(head) {
+			node<T1, T2>* temp = head;
+			head = head->next;
+			delete temp;
+		}
+
+	}
+
+	void rehash() {
+		int oldM = M; // to store the current capacity 
+		node<T1, T2>** oldTable = table; // to store address of current table
+
+		// 1. create a new array with double capacity
+		M = 2*M;
+		table = new node<T1, T2>*[M];	
+		for(int i=0; i<M; i++) {
+			table[i] = NULL;
+		}
+		N = 0;
+
+		// 2. transfer contents from the old array into the new array
+		for(int i=0; i<oldM; i++) {
+			transfer(oldTable[i]);
+		}
+
+		// 3. delete the old array
+		delete [] oldTable;
+	}
+
+	public :
+
+		HashTable(int M=5, double LFT=0.7) {
+			this->M = M;
+			this->LFT = LFT;
+			this->N = 0;
+			table = new node<T1, T2>*[M];	
+			for(int i=0; i<M; i++) {
+				table[i] = NULL;
+			}
+		} 
+
+		void insert(T1 key, T2 value) {
+			// 1. compute the hash index using the key
+			int hash_idx = hash_fn(key);
+
+			// 2. insert the (key, value) pair at the head of the LL at the hash index
+			node<T1, T2>* n = new node<T1, T2>(key, value);
+			n->next = table[hash_idx];
+			table[hash_idx] = n;
+
+			N++;
+
+			// 3. rehash the HT, in case the load factor exceeds the load factor threshold
+			double LF = N/(M*1.0);
+			if(LF > LFT) {
+				// rehash the HT
+				cout << "load factor : " << LF << ", rehashing..." << endl;
+				rehash();
+			}
+		}
+
+		node<T1, T2>* find(T1 key) {
+			// 1. compute the hash index for the given key
+			int hash_idx = hash_fn(key);
+
+			// 2. search for the key in the linked list at the hash_idx
+			node<T1, T2>* temp = table[hash_idx];
+			while(temp && temp->key != key) {
+				temp = temp->next;
+			}
+			return temp;
+		}
+
+		void erase(T1 key) {
+			// 1. compute the hash index corresponding to the key
+			int hash_idx = hash_fn(key);
+
+			// 2. delete the node with the given key, if present, from 
+			//    the linked list at the hash index
+			node<T1, T2>* prev = NULL;
+			node<T1, T2>* temp = table[hash_idx];	
+			if(temp->key == key) {
+				// delete from the head of the linked list
+				table[hash_idx] = temp->next;
+				delete temp;
+				N--;
+			} else {
+				// delete from non-head of the linked list
+				while(temp) {
+					if(temp->key == key) {
+						prev->next = temp->next;
+						delete temp;
+						N--;
+						break;
+					}
+					prev = temp;
+					temp = temp->next;
+				}
+			}
+		}
+
+		void printLinkedList(node<T1, T2>* head) {
+			while(head) {
+				cout << "(" << head->key << ", " << head->value << ")";
+				head = head->next;
+				if(head) cout << "->"; 
+			}
+			cout << endl;
+		}
+
+		void print() {
+			for(int i=0; i<M; i++) {
+				cout << i << " : ";
+				printLinkedList(table[i]);
+			}
+			cout << endl;
+		}
+
+		T2& operator[](T1 key) {
+			node<T1, T2>* n = find(key);
+			if(!n) {
+				// key is not present inside the hash table
+				T2 garbage;
+				insert(key, garbage);
+				n = find(key);
+			}
+			return n->value;
+		}
+};
+
+
+int main() {
+
+	// HashTable<int, int> hashTable;
+
+	// hashTable.insert(0, 0);
+	// hashTable.insert(10, 100);
+	// hashTable.insert(1, 1);
+	// hashTable.insert(11, 121);
+	// hashTable.insert(13, 169);
+
+	// hashTable.print();
+
+	// hashTable.erase(0);
+
+	// hashTable.print();
+
+	// node<int, int>* node = hashTable.find(13);
+	// node ? cout << "(" << node->key << ", " << node->value << ")" << endl :
+	//        cout << "not found!" << endl;
+
+	// hashTable.erase(13);
+
+	// hashTable.print();
+
+	// node = hashTable.find(13);
+	// node ? cout << "(" << node->key << ", " << node->value << ")" << endl :
+	//        cout << "not found!" << endl;
+
+	HashTable<int, int> hashTable;
+
+	hashTable.insert(0, 0);
+	hashTable.insert(10, 100);
+	hashTable.insert(1, 1);
+	
+	hashTable.print();
+
+	hashTable.insert(11, 121); // N = 4, M = 5, 4/5 = 0.8 > 0.7
+	
+	hashTable.print();
+
+	hashTable.insert(13, 169);
+
+	hashTable[14] = 0; // hashTable.operator[](14) = 0;
+	hashTable[14] += 196; 
+	
+	hashTable.print();
+ 			
+}
+
+
+
+
+
+
+
